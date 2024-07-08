5d638305eb27ee170c3af95797e5b4608bec7d76
MShreyas1 <shreyasmuralidh@gmail.com>
2024-07-07 22:46:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var images = document.querySelectorAll(".zoom-image");
-    var overlay = document.getElementById("overlay");
-    var expandedImg = document.getElementById("expandedImg");
-    var closeBtn = document.getElementById("closeBtn");
+    var imageContainer = document.getElementById("imageContainer");
+    var images = Array.from(imageContainer.getElementsByClassName("zoom-image"));
 
-    images.forEach(function(img) {
-        img.onclick = function() {
-            overlay.style.display = "flex";
-            expandedImg.src = this.getAttribute("data-highres");
+    // Function to shuffle an array
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
         }
+    }
+
+    // Shuffle the images
+    shuffle(images);
+
+    // Remove all images from the container
+    images.forEach(function(image) {
+        imageContainer.removeChild(image);
     });
 
-    closeBtn.onclick = function() {
-        overlay.style.display = "none";
-    }
-});
+    // Append images back to the container in shuffled order
+    images.forEach(function(image) {
+        imageContainer.appendChild(image);
+    });
