3465d95e9d091c7ccf9f5483db674d983d112080
MShreyas1 <shreyasmuralidh@gmail.com>
2024-07-07 21:47:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var img = document.getElementById("myImage");
+    var images = document.querySelectorAll(".zoom-image");
     var overlay = document.getElementById("overlay");
     var expandedImg = document.getElementById("expandedImg");
     var closeBtn = document.getElementById("closeBtn");
 
-    img.onclick = function(){
-        overlay.style.display = "flex";
-        expandedImg.src = this.getAttribute("data-highres");
-    }
+    images.forEach(function(img) {
+        img.onclick = function() {
+            overlay.style.display = "flex";
+            expandedImg.src = this.getAttribute("data-highres");
+        }
+    });
 
-    closeBtn.onclick = function() { 
+    closeBtn.onclick = function() {
         overlay.style.display = "none";
     }
 });
